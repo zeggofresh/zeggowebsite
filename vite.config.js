fbc673877d0b5b7f8e67b4463342785316d54c47
zeggofresh <zeggofresh@gmail.com>
2025-12-27 13:31:17 +0530

    dfdf

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,7 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
 export default defineConfig({
   plugins: [react()],
-  base: "/zeggowebsite/",
+  base: "/zeggowebsite/",   // 👈 ye line add karni hai
 });
